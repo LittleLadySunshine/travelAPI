7703f79c95a82896e9a5781aa10d9295cbf4fd94
Denise McCoy <Denise.L.McCoy@gmail.com>
2017-03-25 16:31:47 -0600

    spec to include router container

--- src/containers/App/App.spec.js
+++ src/containers/App/App.spec.js
@@ -7,18 +7,27 @@ import styles from  './styles.module.css'
 
 describe('<App />', function() {
   let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<App />)
-  });
+    let history = {};
+    beforeEach(() => {
+      wrapper =
+        shallow(<App history={history}/>)
+    })
 
-  it('has a single wrapper element', () => {
-    expect(wrapper.find('.${styles.wrapper}'))
-      .to.have.length(0);
+  it('has a Router component', () => {
+    expect(wrapper.find('Router'))
+      .to.have.length(1);
   });
 
   it('has a Router component', () => {
   expect(wrapper.find('Router'))
       .to.have.length(1);
   });
+
+  it('passes a history prop', () => {
+  const props = wrapper.find('Router').props();
+
+  expect(props.history)
+    .to.be.defined;
+  })
 
 });
