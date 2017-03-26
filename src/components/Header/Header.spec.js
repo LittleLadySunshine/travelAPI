ce931e05929944ee8e3e1283916d830cc300257c
Denise McCoy <Denise.L.McCoy@gmail.com>
2017-03-25 18:12:01 -0600

    added more to spec header file

--- src/components/Header/Header.spec.js
+++ src/components/Header/Header.spec.js
@@ -5,4 +5,21 @@
   import Header from './Header'
   import styles from './styles.module.css'
 
-  
+  describe('<Header />', () => {
+    let wrapper;
+    beforeEach(() =? {
+      wrapper = shallow(<Header />)
+    });
+
+    it('contains a title component with yelp' () => {
+      expect(wrapper.find('h1').first().text())
+        .to.equal('Yelp')
+    });
+
+    it ('contains topbar styling', () => {
+      expect(wrapper.find('.${styles.topbar}'))
+        .to.have.length(1)
+    });
+
+    
+  })
