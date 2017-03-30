665c988ad548de96653ab2c2966076825210f2bb
Denise McCoy <Denise.L.McCoy@gmail.com>
2017-03-30 13:07:29 -0600

    set up import

---
+++ src/components/Listing/Item.js
@@ -0,0 +1,17 @@
+import React, { PropTypes as T } from 'react'
+import classnames from 'classnames'
+
+import Rating from 'components/Rating/Rating';
+import styles from './styles.module.css'
+
+
+
+
+
+
+
+
+
+
+
+export default Item
