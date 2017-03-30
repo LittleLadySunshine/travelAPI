eafe0f8bcb36cbcb32f0c82a21fc1457c042dee8
Denise McCoy <Denise.L.McCoy@gmail.com>
2017-03-30 14:25:43 -0600

    added wrapper

--- src/components/Listing/Item.spec.js
+++ src/components/Listing/Item.spec.js
@@ -4,3 +4,12 @@ import { shallow } from 'enzyme'
 
 import Item from './Item'
 import styles from './styles.module.css'
+
+describe('<Item />', () => {
+  let wrapper;
+  const place = {
+    name: 'bob'
+  }
+  beforeEach(() => {
+    wrapper = shallow(<Item place={place} />)
+  });
