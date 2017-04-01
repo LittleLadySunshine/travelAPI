c74fd4c6a95357cedd8b21d0a071f5e686d4738b
Denise McCoy <Denise.L.McCoy@gmail.com>
2017-04-01 17:12:50 -0600

    added rating spec

--- src/components/Listing/Item.spec.js
+++ src/components/Listing/Item.spec.js
@@ -23,4 +23,11 @@ describe('<Item />', () => {
   it('wraps the component with an .item css class', () => {
     expect(wrapper.find(`.${styles.item}`))
       .to.have.length(1);
-  })
+  });
+
+  it('contains a rating', () => {
+  expect(wrapper.find('Rating'))
+    .to.be.defined;
+  });
+
+});
